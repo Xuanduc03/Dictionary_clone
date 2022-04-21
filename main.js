1fe440b176c94f535b9d57e014678fe9dda32afb
mai duc <maiduc688@gmail.com>
2022-04-21 20:22:17 +0700

    commit on 'master'

--- main.js
+++ main.js
@@ -18,18 +18,13 @@ function data(result, word){
         document.querySelector(".word span").innerText = phontetics;
         document.querySelector(".meaning span").innerText = definitions.definition;
         document.querySelector(".example span").innerText = definitions.example;
-        audio = new Audio("https:" + result[0].phonetics[0].audio);
+        audio = new Audio(result[0].phonetics[0].audio);
 
         if(definitions.synonyms[0] = undefined){
             synonyms.parentElement.style.display = "none";
         }else{
             synonyms.parentElement.style.display = "block";
-            synonyms.innerHTML = "";
-            for (let i = 0; i < 5; i++) {
-                let tag = `<span onclick="search('${definitions.synonyms[i]}')">${definitions.synonyms[i]},</span>`;
-                tag = i == 4 ? tag = `<span onclick="search('${definitions.synonyms[i]}')">${definitions.synonyms[4]}</span>` : tag;
-                synonyms.insertAdjacentHTML("beforeend", tag);
-            }
+            document.querySelector(".synonyms .list").innerText = result[0].meanings[3].synonyms.join(", ");
         }
     }
 }
